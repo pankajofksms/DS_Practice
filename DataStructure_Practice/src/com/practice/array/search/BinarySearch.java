d83e8a41fe3dab189c699d5e2509d7cc75237cb7
pankaj <pankaj@192.168.1.9>
2020-05-25 12:13:43 +0530

    Refactor binary search code

--- DataStructure_Practice/src/com/practice/array/search/BinarySearch.java
+++ DataStructure_Practice/src/com/practice/array/search/BinarySearch.java
@@ -4,16 +4,16 @@ public class BinarySearch {
 
 	public static void main(String[] args) {
 
-		int arr[] = {1,2,3,4,5,6,7,8,9};
+		int arr[] = {1,2,3,4,5,6,7,8};
 		
-		System.out.println(binarySearch(arr, 6)); 
+		System.out.println(binarySearch(arr, 4)); 
 	}
 
 	private static boolean binarySearch(int arr[], int numberToSearch) {
 		int low = 0;
-		int high = arr.length - 1;
+		int high = arr.length;
 		int medium;
-		for (int i = low; i <= high; i++) {
+		while(low<=high) {
 			medium = (low + high) / 2;
 			if (arr[medium] == numberToSearch)
 				return true;
